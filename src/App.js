6c985f88d7bfea2ac23aba7ac139e98b638ee23d
Hameed Akshal <mailmeakshal@gmail.com>
2023-11-08 20:53:08 +0530

    change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Hello World by Hameed Akshal</code>
+          <code>Hello World by Hameed Akshal for HAJAR</code>
         </p>
         <a
           className="App-link"
